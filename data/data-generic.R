91bba96dd048a29d1d3a3508c10e7670d24ce58e
girordo <tsgiroldo@gmail.com>
2023-08-07 18:23:52 -0300

    feat(more data): adding more data

--- data/data-generic.R
+++ data/data-generic.R
@@ -1,13 +1,21 @@
 skills <- tribble(
   ~area, ~skills, ~detail,
-  "Tecnologia", "Javascript", "Experiência com Javascript em projetos front-end usando React, Redux e Jest.",
-  "Tecnologia", "Typescript", "Uso do Typescript com Prisma para acesso a bancos de dados.",
-  "Tecnologia", "Elixir", "Exploração de Elixir em projetos pessoais usando Phoenix e Ecto.",
-  "Tecnologia", "HTML", "Criação de páginas web semânticas e acessíveis.",
-  "Tecnologia", "CSS", "Estilização de projetos usando SASS e metodologia BEM.",
-  "Tecnologia", "Rust", "Aprendizado de Rust para projetos secundários.",
-  "Tecnologia", "DevOps", "Experiência com Docker e integração contínua usando Jenkins.",
-  "Tecnologia", "Bancos de Dados", "Trabalho com bancos de dados SQL e NoSQL.",
+  "Tecnologia", "Javascript", "Experiência com Javascript em projetos front-end usando React",
+  "Tecnologia", "Javascript", "Redux, Redux Toolkit e Context API para gerencia de estados",
+  "Tecnologia", "Javascript", "Jest e React Testing Library para testes unitários",
+  "Tecnologia", "Javascript", "Cypress para testes de integração",
+  NA, "Typescript", "Zod para validação de schemas",
+  NA, "Typescript", "Uso do Typescript com Drizzle para acesso a bancos de dados.",
+  NA, "Elixir", "Exploração de Elixir em projetos pessoais usando Phoenix e Ecto.",
+  NA, "Elixir", "Utilizando LiveView para aplicações fullstack.",
+  NA, "HTML", "Criação de páginas web semânticas e acessíveis.",
+  NA, "CSS", "Estilização de projetos usando SASS e metodologia BEM.",
+  NA, "Rust", "Aprendizado de Rust para projetos secundários.",
+  NA, "Rust", "Seguindo o livro zero2prod",
+  NA, "DevOps", "Experiência com virtualização e conteineres utilizando Docker",
+  NA, "DevOps", "CI/CD com Github Actions e TravisCI",
+  NA, "Bancos de Dados", "Trabalho com bancos de dados SQL e NoSQL.",
+  NA, "Bancos de Dados", "MongoDB para bancos não relacionais",
   "Linguagens", "Português", "Língua nativa",
   "Linguagens", "Inglês", "Fluente",
 )
@@ -15,18 +23,23 @@ skills <- tribble(
 edu <- tribble(
   ~degree, ~startYear, ~endYear, ~inst, ~where, ~detail,
   "Bacharelado em Ciência da Computação", 2015, 2019, "Universidade Imaginária", "Cidade Fictícia, Estado Fictício, País Fictício", "Estudos em algoritmos, estruturas de dados e programação orientada a objetos.",
+  "Bacharelado em Ciência da Computação", 2015, 2019, "Universidade Imaginária", "Banco de dados NoSQL, utilização de Python para análise de dados, aulas de machine learning e reconhecimento de padrões",
+  "Mestrado em Engenharia de Software", 2020, 2022, "Universidade Fictícia", "Cidade Fictícia, Estado Fictício, País Fictício", "Nota: 9.4",
   "Mestrado em Engenharia de Software", 2020, 2022, "Universidade Fictícia", "Cidade Fictícia, Estado Fictício, País Fictício", "Pesquisa em desenvolvimento de software, foco em metodologias ágeis e arquiteturas escaláveis.",
 )
 
 work <- tribble(
   ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
   "Engenheiro de Software", "TechSoft", "Janeiro", 2020, "Dezembro", 2022, "Cidade Fictícia, Estado Fictício", "Trabalhei como parte de uma equipe no desenvolvimento de aplicativos web usando React e Redux.",
-  "Desenvolvedor Full Stack", "WebTech", "Março", 2018, "Novembro", 2019, "Cidade Fictícia, Estado Fictício", "Participei do desenvolvimento de um aplicativo full stack utilizando Node.js, React e MongoDB.",
+  "Engenheiro de Software", "FastWeb", "Janeiro", 2020, "Dezembro", 2022, "Cidade Fictícia, Estado Fictício", "Trabalhei como parte de uma equipe no desenvolvimento de aplicativos web usando React e Redux.",
+  "Desenvolvedor Full Stack II", "WebTech", "Novembro", 2019, "Janeiro", 2020, "Cidade Fictícia, Estado Fictício", "Participei do desenvolvimento de um aplicativo full stack utilizando Node.js, React e MongoDB.",
+  "Desenvolvedor Full Stack 9 ", "WebTech", "Março", 2018, "Novembro", 2019, "Cidade Fictícia, Estado Fictício", "Participei do desenvolvimento de um aplicativo full stack utilizando Node.js, React e MongoDB.",
   "Estagiário de Desenvolvimento", "ByteCorp", "Julho", 2017, "Dezembro", 2017, "Cidade Fictícia, Estado Fictício", "Ganhei experiência prática trabalhando em projetos de desenvolvimento de software.",
 )
 
 honors <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
+  "Hackathon", "Melhor Aluno de Computação", 2021, "Wall Street", "Premiado com 1 bilhão de dólares.",
   "Reconhecimento Acadêmico", "Melhor Aluno de Computação", 2019, "Universidade Imaginária", "Premiado por excelência acadêmica na área de Computação.",
   "Competição de Programação", "1º Lugar em Algoritmos", 2018, "Evento de Programação Fictício", "Conquista do primeiro lugar na competição de programação de algoritmos.",
 )
@@ -41,4 +54,5 @@ certifications <- tribble(
   ~area, ~accomplishment, ~year, ~where, ~detail,
   "Certificações Profissionais", "Certificação em React", 2021, "Plataforma de Ensino Online Fictícia", "Obtenção da certificação em React para comprovar habilidades na biblioteca.",
   "Certificações Profissionais", "Certificação em Docker", 2020, "Plataforma de Ensino Online Fictícia", "Conquista da certificação em Docker para demonstrar conhecimentos em contêineres.",
+  "Certificações Profissionais", "TDD with React", 2020, "Plataforma de Ensino Online Fictícia", "Conquista da certificação em TDD utilizando React para demonstrar conhecimentos em contêineres.",
 )
