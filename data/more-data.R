44eabc80321fe3b0e309d2de356682a8e61387d5
girordo <tsgiroldo@gmail.com>
2024-02-22 20:17:50 -0300

    feat(data/more-data.R): adding more data

---
+++ data/more-data.R
@@ -0,0 +1,25 @@
+React, 5 anos, trabalho com React e seu ecossistema envolvendo Native, NextJS, Redux, Context API, Hooks, Zustand, styled components, AntDesign e TailwindCSS
+
+Criação de componentes e páginas reutilizáveis, com fácil adaptação e a depender do projeto fortemente tipado ou não (se JS utilizando PropTypes).
+
+Tanto escrevendo class components como functional components.
+
+Escrevendo tipos e derivando os mesmos utilizando Typescript
+
+Utilizando Tanstack Query para gerenciamento de estados assíncronos
+
+Testes unitários com Jest e React Testing Library, testes de integração envolvendo 
+Cypress e Playwright.
+
+Otimização da performance utilização SWC, esbuild.
+
+Deploy de aplicações e melhoria do processo de integração contínua e deploy contínuo utilizando Github Actions, Jenkins e CircleCI, utilizando ferramentas para averiguar a segurança e qualidade do código além de implementar os testes ao código utilizando Sonarqube, Fortify e Snyk.
+
+Também já trabalhei tanto com SSR, ISG e SSG com NextJS e Gatsby
+
+Integração de APIs terceiras utilizando fetch ou axios.
+Utilizando JWT para autenticações a depender do projeto (em alguns NextAuth).
+
+Trabalhei com NextJS escrevendo também a API para integração.
+
+Particularmente sempre utilizando eslint e prettier em todos os projetos.
